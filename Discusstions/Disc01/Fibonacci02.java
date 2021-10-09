890b15c74e91c24049ab1d831fd75eca53441f9c
voyageyang <583992171@qq.com>
2021-10-09 22:55:33 +0800

    cs61b discussion programs

---
+++ Discusstions/Disc01/Fibonacci02.java
@@ -0,0 +1,10 @@
+public class Fibonacci02 {
+	public static void main(String[] args) {
+		System.out.println(fib2(5, 1, 0, 1));
+	}
+
+	public static int fib2(int n, int k, int f0, int f1) {
+		if(n == k) return f0;
+		else return fib2(n, k + 1, f1, f0 + f1);
+	}
+}
---
+++ Discusstions/Disc02/SquraingAList.java
@@ -0,0 +1,12 @@
+public class SquraingAList {
+    /*public static void main(String[] args) {
+
+    }
+
+    public static IntList square(IntList L) {
+
+    }
+    public static IntList squareMutative(IntList L) {
+
+    }*/
+}
